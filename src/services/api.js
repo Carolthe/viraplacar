0b2765f0a293a5f24acb56eead7459daf69cdd5e
Caroline Tenorio <carololiveiratenorio21@gmail.com>
2026-04-17 17:40:02 +0100

    Atu

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
-// backend/src/services/api.js ou frontend/src/services/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://backend-production-4a532.up.railway.app/api", // usa a variável do .env
-  withCredentials: true, // se quiser enviar cookies HttpOnly
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
+  withCredentials: true,
 });
 
 export default api;
